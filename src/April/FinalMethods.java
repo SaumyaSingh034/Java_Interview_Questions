021fb5261d7f5bccca65385e5cf266083e12845d
Saumya <singhsaumyait034@gmail.com>
2025-04-05 18:23:12 +0530

    Flipkart and Amazon

---
+++ src/April/FinalMethods.java
@@ -0,0 +1,19 @@
+package April;
+
+public class FinalMethods {
+    public final void display(){
+        System.out.println("Display");
+    }
+
+    public final void display(String s){
+        System.out.println("Display "+s);
+    }
+    public FinalMethods(){
+
+    }
+
+    public FinalMethods(String s){
+        System.out.println("Constructor");
+
+    }
+}

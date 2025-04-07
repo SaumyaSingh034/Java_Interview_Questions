a167f294f8a03093e4bee72318ea5e810f881021
Saumya <singhsaumyait034@gmail.com>
2025-04-07 22:02:07 +0530

    Reverse String

---
+++ src/April/StringManipulation4.java
@@ -0,0 +1,30 @@
+package April;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringManipulation4 {
+    public static void main(String[] args){
+        String input = "abc-2019";
+        stringManipulation4(input);
+    }
+
+    private static void stringManipulation4(String input) {
+        StringBuilder stringData = new StringBuilder();
+        StringBuilder digitData = new StringBuilder();
+        stringData.append("[");
+        digitData.append("[");
+        for(char c : input.toCharArray()){
+            if(Character.isAlphabetic(c))
+                stringData.append(c);
+            else if (Character.isDigit(c)) {
+                digitData.append(c);
+
+            }
+        }
+        stringData.append("]");
+        digitData.append("]");
+        System.out.println(stringData);
+        System.out.println(digitData);
+    }
+}

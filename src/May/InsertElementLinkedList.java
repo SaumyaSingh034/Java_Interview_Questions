bd650d9440e6c80f96005d754ef175e7edba5adc
Saumya <singhsaumyait034@gmail.com>
2025-05-05 11:30:40 +0530

    Insert Element Linked List

--- src/May/InsertElementLinkedList.java
+++ src/May/InsertElementLinkedList.java
@@ -1,4 +1,22 @@
 package May;
 
+import java.util.LinkedList;
+
 public class InsertElementLinkedList {
+    public static void main(String[] args){
+        LinkedList<String> linkedData = new LinkedList<>();
+        linkedData.add("Test1");
+        linkedData.add("Test1");
+        linkedData.add("Test2");
+        linkedData.add("Test3");
+        linkedData.add("Test4");
+        linkedData.add("Test5");
+        System.out.println(linkedData);
+       int index=  linkedData.indexOf("Test1");
+       linkedData.add(index, "ReplacedTest");
+       System.out.println(linkedData);
+       linkedData.set(4,"SaumyaLinkedList");
+        System.out.println(linkedData);
+
+    }
 }

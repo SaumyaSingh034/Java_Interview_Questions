b9e031f15b435a252ae04ebdf49135cc22c658fb
Saumya <singhsaumyait034@gmail.com>
2025-05-14 16:11:34 +0530

    Swap Or reverse the words

--- src/May/InterviewQuestions2025/OPTUM_SwapTwoWords.java
+++ src/May/InterviewQuestions2025/OPTUM_SwapTwoWords.java
@@ -2,5 +2,16 @@ package May.InterviewQuestions2025;
 
 public class OPTUM_SwapTwoWords {
    public static void main(String[] args){
+       String str = "Hello World";
+       swapTwoWords(str);
    }
+
+    private static void swapTwoWords(String str) {
+       StringBuilder result = new StringBuilder();
+       String[] words = str.split("\\s+");
+       for(int i = words.length-1;i>=0;i--){
+           result.append(words[i]).append(" ");
+       }
+       System.out.println(result.toString().trim());
+    }
 }

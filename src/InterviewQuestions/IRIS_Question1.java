72b0c4efa36800cf8b9d5c25be80c8eed677b934
Saumya <singhsaumyait034@gmail.com>
2025-05-24 12:10:56 +0530

    IRIS - Round 1

--- src/InterviewQuestions/IRIS_Question1.java
+++ src/InterviewQuestions/IRIS_Question1.java
@@ -1,5 +1,20 @@
 package InterviewQuestions;
 
 public class IRIS_Question1 {
+    public static void main(String[] args){
+        String str = "0000012350609";
+        removeTrailingZero(str);
+    }
 
+    private static void removeTrailingZero(String str) {
+        char[] ch = str.toCharArray();
+        int index = 0;
+        for(int i=0;i< ch.length;i++){
+            if(ch[i]!='0'){
+                index = i;
+                break;
+            }
+        }
+        System.out.println(str.substring(index));
+    }
 }

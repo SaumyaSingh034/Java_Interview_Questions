29c7ad1e3053a40e24e1700586940e2e51519e20
Saumya <singhsaumyait034@gmail.com>
2025-05-26 23:56:12 +0530

    NAB Round 2

--- src/InterviewQuestions/NAB_Round2.java
+++ src/InterviewQuestions/NAB_Round2.java
@@ -1,4 +1,27 @@
 package InterviewQuestions;
 
-public class NAB_Round2 {
+import java.util.HashMap;
+import java.util.Map;
+
+public class NAB_Round2 { public static void main(String[] args){
+    String str = "philadelphia is a place ";
+    printOnLyVowels(str);
 }
+
+    private static void printOnLyVowels(String str) {
+        str = str.replaceAll(" ","");
+        Map<Character, Integer> data = new HashMap<>();
+
+        for(char c : str.toCharArray()){
+            data.put(c, data.getOrDefault(c, 0)+1);
+        }
+
+        for(Map.Entry<Character, Integer> hm : data.entrySet()){
+            if(hm.getKey() == 'a' || hm.getKey() == 'e' ||
+                    hm.getKey() =='i' || hm.getKey() == 'o' || hm.getKey() == 'u'){
+                System.out.println(hm.getKey()+ " ----> "+hm.getValue());
+            }
+        }
+    }
+}
+

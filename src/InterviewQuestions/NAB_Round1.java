6e119fc6107960c4f3695438e4209063d0d61948
Saumya <singhsaumyait034@gmail.com>
2025-05-26 23:55:12 +0530

    NAB-Round1

--- src/InterviewQuestions/NAB_Round1.java
+++ src/InterviewQuestions/NAB_Round1.java
@@ -1,4 +1,23 @@
 package InterviewQuestions;
 
 public class NAB_Round1 {
+    public static void main(String[] args){
+        String str = "This is a sunny day";
+        //Reverse String preserving whitespaces
+        reverseStringPreserving(str);
+    }
+
+    private static void reverseStringPreserving(String str) {
+
+        String str1 = str.replaceAll(" ","");
+        StringBuilder result = new StringBuilder(str1);
+        result.reverse();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == ' ') {
+                result.insert(i, ' ');
+            }
+        }
+        System.out.println(result);
+
+    }
 }

3b93c45bb7d1a80a39bad55700c4eab8291b3884
Saumya <singhsaumyait034@gmail.com>
2025-05-27 00:04:46 +0530

    Get Sum of Digits in String

--- src/InterviewQuestions/AKHIRS_Question2.java
+++ src/InterviewQuestions/AKHIRS_Question2.java
@@ -1,4 +1,25 @@
 package InterviewQuestions;
 
+
 public class AKHIRS_Question2 {
+    public static void main(String[] args){
+        String str = "saumya23456&*^yu2";
+        fetchNumberAndSum(str);
+    }
+
+    private static void fetchNumberAndSum(String str) {
+        int sum = 0;
+        int digit = 0;
+        for(char c : str.toCharArray()){
+            if(Character.isDigit(c)){
+                digit = Character.getNumericValue(c);
+                sum += digit;
+            }
+        }
+        System.out.println(sum);
+    }
+
+
+
+
 }

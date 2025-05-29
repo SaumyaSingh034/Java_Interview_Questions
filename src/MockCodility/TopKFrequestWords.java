a7f91b1b940c8736c21a1f6601d8393265d45410
Saumya <singhsaumyait034@gmail.com>
2025-05-29 23:15:43 +0530

    Top K Frequent Words

--- src/MockCodility/TopKFrequestWords.java
+++ src/MockCodility/TopKFrequestWords.java
@@ -18,10 +18,23 @@ public class TopKFrequestWords {
             hashMap.put(s, hashMap.getOrDefault(s, 0)+1);
         }
 
-        for(Map.Entry<String, Integer> hashEntry : hashMap.entrySet()){
-            if(hashEntry.getValue() >= k)
-                result.add(hashEntry.getKey());
+        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(hashMap.entrySet());
+        Collections.sort(entryList, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if(!o1.getValue().equals(o2.getValue())){
+                    return o2.getValue() - o1.getValue();
+                }else{
+                    return o1.getKey().compareTo(o2.getKey());
+                }
+            }
+        });
+
+        List<String> result1 = new ArrayList<>();
+        for(int i=0;i<k && i<entryList.size();i++){
+            result1.add(entryList.get(i).getKey());
         }
-        System.out.println(result);
+        System.out.println(result1);
+
     }
 }

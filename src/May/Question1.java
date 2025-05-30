b61ea13dc796a0d9759a0729e1cb81589c43d7a1
Saumya <singhsaumyait034@gmail.com>
2025-05-30 21:25:01 +0530

    Question 1

--- src/May/Question1.java
+++ src/May/Question1.java
@@ -5,9 +5,27 @@ import java.util.Set;
 
 public class Question1 {
     public static void main(String[] args){
-        String str = "saumyasingh";
+        String str = "saumyasingh"; //umyasingh
         //findUniqueLongestSubstring
         longestUniqueSubstringNonRepeated(str);
+        longestUniqueSubstringNonRepeated1(str);
+    }
+
+    private static void longestUniqueSubstringNonRepeated1(String str) {
+        int maxLenth = 0;
+        int left = 0;
+        Set<Character> data = new HashSet<>();
+        for(int i=0;i<str.length();i++){
+            char c = str.charAt(i);
+            if(!data.contains(c)){
+                data.add(c);
+                maxLenth = Math.max(i-left+1, maxLenth);
+            }else{
+                data.remove(left++);
+            }
+
+        }
+        System.out.println(maxLenth);
     }
 
     private static void longestUniqueSubstringNonRepeated(String str) {

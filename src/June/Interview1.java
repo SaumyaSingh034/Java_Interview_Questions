0e70590792738c073a089b35937ab27bd9597143
SaumyaSingh034 <singhsaumyait034@gmail.com>
2025-06-10 11:00:48 +0530

    Initial Round of coding

--- src/June/Interview1.java
+++ src/June/Interview1.java
@@ -1,4 +1,14 @@
 package June;
 
 public class Interview1 {
+    public static void main(String[] args){
+        int number = 5;
+        createDynamicString(number);
+    }
+
+    private static void createDynamicString(int number) {
+        while(number>0){
+
+        }
+    }
 }

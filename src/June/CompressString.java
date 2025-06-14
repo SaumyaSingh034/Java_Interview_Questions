6f3922a517afc65f1e639a8132ca8b29239687e4
SaumyaSingh034 <singhsaumyait034@gmail.com>
2025-06-14 19:53:38 +0530

    Compress String

--- src/June/CompressString.java
+++ src/June/CompressString.java
@@ -1,9 +1,29 @@
 package June;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class CompressString {
     public static void main(String[] args){
         String str = "aaabbbbccdd";
         //o/p --> a3b4c2d2
 
+        compressString11(str);
+
+    }
+
+    private static void compressString11(String str) {
+        Map<Character, Integer> data = new LinkedHashMap<>();
+        StringBuilder result = new StringBuilder();
+
+        for(char c : str.toCharArray()){
+            data.put(c, data.getOrDefault(c,0)+1);
+        }
+
+        for(Map.Entry<Character, Integer> dataSet : data.entrySet()){
+            result.append(dataSet.getKey()).append(dataSet.getValue());
+        }
+
+        System.out.println(result.toString());
     }
 }

d41e58f23d8d5c01d4f60aac9c5817b48e100229
SaumyaSingh034 <singhsaumyait034@gmail.com>
2025-06-15 23:44:10 +0530

    Map Payload Creation

--- src/RestAssuredPayload/MapPayloadCreation.java
+++ src/RestAssuredPayload/MapPayloadCreation.java
@@ -1,4 +1,15 @@
 package RestAssuredPayload;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapPayloadCreation {
+    public static void main(String[] args){
+        Map<String, String> data = new HashMap<>();
+        data.put("Saumya", "Test Automation Engineer");
+        data.put("Rahul", "Senior Cloud Admin Engineer");
+        data.put("Nandini", "Senior Software Quality Engineer");
+
+        System.out.println(data);
+    }
 }
